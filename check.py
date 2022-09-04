0eeb5f5ff38068a15c8224ff96010e2b14cebc59
plun1331 <plun1331@gmail.com>
2022-09-04 14:17:16 -0700

    feat: check for translation consistency

--- check.py
+++ check.py
@@ -1,10 +1,36 @@
 import json
 
-with open('encodings.json', 'r') as f:
-    encodings = json.load(f)
-    
+with open('encodings.json', 'r') as ef:
+    encodings = json.load(ef)
+
+# load in the base translation file (en-US)
+with open(f'translations/en-US.json', 'r', encoding='utf-8') as file:
+    en_us = json.load(file)
+
+
+def match_keys(original: dict, data: dict, *, path: str):
+    original_keys = set(original.keys())
+    keys = set(data.keys())
+    if original_keys != keys:
+        missing = original_keys - keys
+        print(f"Missing keys in {path}: " + ', '.join(missing))
+    for k, v in data.items():
+        if not isinstance(v, type(original[k])):
+            print(f"Incorrect object type at {path}/{k}")
+            continue
+        if isinstance(v, dict):
+            match_keys(original[k], v, path=path + '/' + k)
+        elif isinstance(v, list):
+            if len(original[k]) != len(v):
+                print(f"Incorrect list length at {path}/{k}")
+
+
+def check_file(locale, enc):
+    with open(f'translations/{locale}.json', 'r', encoding=enc) as f:
+        data = json.load(f)
+    match_keys(en_us, data, path=locale)
+    print(f'Completed check for {locale}')
+
 
 for lang, encoding in encodings.items():
-    with open(f'translations/{lang}.json', 'r', encoding=encoding) as f:
-        json.load(f)
-        print(f'translations/{lang}.json OK')
+    check_file(lang, encoding)
